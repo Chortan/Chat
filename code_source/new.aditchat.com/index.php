e09ce4c58b46b57ae3b7cdf1db24fb12e15d9acf
jordan <jordan.roimarmier@outlook.com>
2015-12-07 15:26:10 +0100

    L'index à été mis à jours

--- code_source/new.aditchat.com/index.php
+++ code_source/new.aditchat.com/index.php
@@ -1,34 +1,31 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors','On');
-	
-	
-	/* INCLUDE */
-	include("scripts/bdd/connect.php");
-	include("scripts/functions.php");
-	require($_SERVER["DOCUMENT_ROOT"]."/scripts/class/User.php");
-	require($_SERVER["DOCUMENT_ROOT"]."/scripts/class/Canal.php");
-	require($_SERVER["DOCUMENT_ROOT"]."/scripts/class/Message.php");
-	
-	
-	require($_SERVER["DOCUMENT_ROOT"]."/scripts/class/Form/Form.php");
-	require($_SERVER["DOCUMENT_ROOT"]."/scripts/class/Form/Element.php");
-	
-	session_start();
-	
-	
-	if(isset($_GET["page"])){
-		$page="Pages/".$_GET["page"]."/".$_GET["page"].".php";
-		if(isset($_GET["subPage"])){
-			$page="Pages/".$_GET["page"]."/".$_GET["subPage"].".php";
-		}
-		if(!file_exists($page)){
-			$_SESSION["erreur"][404]=" La page '".$_GET["page"]."/".$_GET["subPage"]."' est introuvable.";
-			header("Location: /Erreur");
-		}
-	}else{
-		header("Location: /Portail");
-	}
+    // Activation des erreur
+    error_reporting(E_ALL);
+    ini_set('display_errors','On');
+    
+    //Fonction
+    include("controlleur/function.php");
+    
+    //Activation des session
+    session_start();
+
+
+    if(!isset($_GET["page"])){
+        header("Location: /Portail");
+    }else if(isset($_GET["page"])){
+        $page = strtolower($_GET["page"]);
+        $pagePath = "vue/page/".$page."/".$page.".php";
+        if(file_exists($pagePath)){
+            include($pagePath);
+        }
+    }else if(isset($_GET["page"]) AND isset($_GET["subpage"])){
+        $page = strtolower($_GET["page"]);
+        $subpage = strtolower($_GET["subpage"]);
+        $pagePath = "vue/page/".$page."/".$subpage . ".php";
+        if(file_exists($pagePath)){
+            include($pagePath);
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -36,16 +33,13 @@
 	<head>
 		<title>T'chat - gratuit</title>
 		<script src="/scripts/jquery.js"></script>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />		<?php 
-			require("scripts/style/style.php"); 
-			header( 'content-type: text/html; charset=utf-8' );
-		?>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	</head>
 	
 	<body>
 		<section class="page">
 			<?php
-				include($page);
+				include($pagePath);
 			?>
 		</section>
 	</body>
